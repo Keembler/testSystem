887be036995cd160f68335a94dadecf9b741e15b
Viktor Shmer <viktorshmer@gmail.com>
2019-04-10 00:12:12 +0700

    Настроил вывод тестовых данных по тестам

--- index.php
+++ index.php
@@ -79,6 +79,25 @@ include('controllers/users.php');
 include('controllers/tests.php');
 include('controllers/questions.php');
 
+function get_test_data($test_id,$link){
+	if(!$test_id) return;
+	$query = mysqli_query($link, "SELECT q.question, q.parent_test, a.id, a.answer, a.parent_question FROM questions q LEFT JOIN answers a ON q.id = a.parent_question WHERE q.parent_test = $test_id");
+	$data = null;
+	while ($row = mysqli_fetch_assoc($query)) {
+		$data[$row['parent_question']][0] = $row['question'];
+		$data[$row['parent_question']][$row['id']] = $row['answer'];
+	}
+	return $data;
+}
+
+if(isset($_GET['test']) and $_GET['test'] !== ''){
+	$test_id = (int)$_GET['test'];
+	$test_data = get_test_data($test_id,$link);
+	/*echo "$test_data";*/
+	include('testing/testirovanie.php');
+	exit;
+}
+
 if ( file_exists('adminpanel/'.$page.'.php') and $_SESSION['$root'] == 1 ) { 
 	include('adminpanel/'.$page.'.php');
 }
--- testing/testirovanie.php
+++ testing/testirovanie.php
@@ -1,35 +1,25 @@
 <?
 include('header.php');
 
-echo "<h2>Выберите тест для прохождения</h2>";
 
-$query = mysqli_query($link, "SELECT * FROM tests");
 
-echo "<div class='list-group'>";
-
-while ($tests = mysqli_fetch_array($query)) {
-	echo "
-		<a href='?test=$tests[id]' class='list-group-item'>$tests[name]</a>
-	";
+if (isset($test_data) && $test_data !== '') {
+	print_r($test_data);
 }
+else {
+	echo "<h2>Выберите тест для прохождения</h2>";
 
-echo "</div><hr>";
+	$query = mysqli_query($link, "SELECT * FROM tests");
 
-function get_test_data($test_id){
-	if(!$test_id) return;
-	$query = mysqli_query($link, "SELECT q.question, q.parent_test, a.id, a.answer, a.parent_question FROM questions q LEFT JOIN answers a ON q.id = a.parent_question WHERE q.parent_test = $test_id");
-	$data = null;
-	while ($row = mysqli_fetch_assoc($query)) {
-		$data[$row['parent_question']][0] = $row['question'];
-		$data[$row['parent_question']][$row['id']] = $row['answer'];
+	echo "<div class='list-group'>";
+
+	while ($tests = mysqli_fetch_array($query)) {
+		echo "
+			<a href='?test=$tests[id]' class='list-group-item'>$tests[name]</a>
+		";
 	}
-	return $data;
-}
 
-if(isset($_GET['test'])){
-	$test_id = (int)$_GET['test'];
-	$test_data = get_test_data($test_id);
-	echo "$test_data";
+	echo "</div><hr>";
 }
 
 
